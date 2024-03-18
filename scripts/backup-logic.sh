d21829aaa478e7ecb974944c0fee4ef15a2e413b
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2024-03-18 15:21:31 +0200

    JE-70937 [DB Backup/Restore add-on]: Restoring to Galera cluster is failed -> Job for mariadb.service failed because a fatal signal was delivered to the control process

--- scripts/backup-logic.sh
+++ scripts/backup-logic.sh
@@ -149,8 +149,20 @@ function backup(){
 	fi
         mongodump ${SSL_TLS_OPTIONS} --uri="mongodb://${DBUSER}:${DBPASSWD}@localhost${RS_SUFFIX}"
     else
-        mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" || { echo "DB credentials specified in add-on settings are incorrect!"; exit 1; }
-        mysqldump -h localhost -u ${DBUSER} -p${DBPASSWD} --force --single-transaction --quote-names --opt --all-databases > db_backup.sql || { echo "DB backup process failed."; exit 1; }
+        SERVER_IP_ADDR=$(ip a | grep -A1 venet0 | grep inet | awk '{print $2}'| sed 's/\/[0-9]*//g' | tail -n 1)
+        [ -n "${SERVER_IP_ADDR}" ] || SERVER_IP_ADDR="localhost"
+        if which mariadb 2>/dev/null; then
+            CLIENT_APP="mariadb"
+        else
+            CLIENT_APP="mysql"
+        fi
+        if which mariadb-dump 2>/dev/null; then
+            DUMP_APP="mariadb-dump"
+        else
+            DUMP_APP="mysql"
+        fi
+        ${CLIENT_APP} -h ${SERVER_IP_ADDR} -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" || { echo "DB credentials specified in add-on settings are incorrect!"; exit 1; }
+        ${DUMP_APP} -h ${SERVER_IP_ADDR} -u ${DBUSER} -p${DBPASSWD} --force --single-transaction --quote-names --opt --all-databases > db_backup.sql || { echo "DB backup process failed."; exit 1; }
     fi
     rm -f /var/run/${ENV_NAME}_backup.pid
 }
